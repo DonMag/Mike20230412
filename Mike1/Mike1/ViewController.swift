f8e2d9539eff55e6a2f2407f1594a63cf1192521
DonMag <don_mail_2000@yahoo.com>
2023-04-12 11:12:33 -0400

    Setup

---
+++ Mike1/Mike1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mike1
+//
+//  Created by Don Mag on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
